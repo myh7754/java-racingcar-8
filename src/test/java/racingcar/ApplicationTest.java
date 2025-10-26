2a3d4262e33c552d3ae9114a73cfe2d47ce37bec
myh <myh4755@gmail.com>
2025-10-27 05:19:39 +0900

    test : 입력값 파싱 테스트 추가

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -1,7 +1,11 @@
 package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import racingcar.service.InputParser;
+
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -11,6 +15,13 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
     private static final int STOP = 3;
+    private InputParser inputParser;
+    private static  final String INPUT_MESSAGE = "112,119,소방차,경찰차";
+
+    @BeforeEach
+    void setUp() {
+        inputParser = new InputParser();
+    }
 
     @Test
     void 기능_테스트() {
@@ -35,4 +46,14 @@ class ApplicationTest extends NsTest {
     public void runMain() {
         Application.main(new String[]{});
     }
+
+    @Test
+    public void 입력값_파싱_테스트() {
+        List<String> parsedMessage = inputParser.parseCarNames(INPUT_MESSAGE);
+
+        assertThat(parsedMessage)
+                .hasSize(4)
+                .containsExactly("112","119","소방차","경찰차");
+    }
+
 }
