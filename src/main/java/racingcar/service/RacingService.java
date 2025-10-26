afaaf02d8528e2960efbfd73da1b0da46a44a8a6
myh <myh4755@gmail.com>
2025-10-27 02:13:03 +0900

    feat(service): 경주게임 추가 조건에 따라 자동자 천진 구현

--- src/main/java/racingcar/service/RacingService.java
+++ src/main/java/racingcar/service/RacingService.java
@@ -1,9 +1,19 @@
 package racingcar.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.domain.Cars;
 
 public class RacingService {
     private int generateRandomNumber() {
         return Randoms.pickNumberInRange(0, 9);
+    }
+
+    public void play(int tryCount, Cars car) {
+        for  (int i = 0; i < tryCount; i++) {
+            int randomNumber = generateRandomNumber();
+            if (randomNumber >= 4) {
+                car.move();
+            }
+        }
     }
 }
